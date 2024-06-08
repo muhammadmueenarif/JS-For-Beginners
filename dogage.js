d95bfdebd5353331e4b55816c15cbf24b2a9a3b1
Mueen Arif <muhammadmueenarif786@gmail.com>
2024-06-08 13:13:24 +0500

    lec 08 js comments

--- dogage.js
+++ dogage.js
@@ -1,1 +1,5 @@
 document.getElementById("math").innerHTML=6+3;
+
+// JS Comments. comments are not shown in output. these are to understand the code. 
+//  // is used for single line comments and 
+//  /* */ is used for multi line comments. used for explaining large line of code. 
